12e444f98a3e33fd8e520cb7842f04a69dab7c02
berkay <berkaynet2000@hotmail.com>
2025-07-20 00:15:53 +0300

    Deploy fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import LandingPage from "./components/Landing1";
-import FinancialLandingPage from "./components/Landing2";
 import LandingV3 from "./components/Landing3";
 
 export default function Home() {
   return (
-    // <LandingPage />
-    // <FinancialLandingPage />
     <LandingV3 />
   );
 }
