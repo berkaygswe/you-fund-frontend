68fe549e172f961b741711e9507767eee4779005
berkay <berkaynet2000@hotmail.com>
2025-07-20 12:02:07 +0300

    Config changes for image cloud provider.

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
         hostname: 'flagcdn.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
 };
